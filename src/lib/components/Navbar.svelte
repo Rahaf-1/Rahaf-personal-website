c7decde964f63d59cc442d18fb1d168fabb96008
Rahaf <roofaj1029@gmail.com>
2024-11-20 20:09:29 +0300

    So far So good

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,84 @@
+<script>
+    // @ts-nocheck
+
+    import Button from "./ui/button/button.svelte";
+    import { page } from "$app/stores";
+    import * as Sheet from "$lib/components/ui/sheet";
+    import ThemeSwitch from "./ThemeSwitch.svelte";
+
+    const links = [
+        {
+            text: "HOME",
+            href: "/",
+        },
+        {
+            text: "ABOUT",
+            href: "/About",
+        },
+        {
+            text: "PROJECTS",
+            href: "/Projects",
+        },
+        {
+            text: "CONTACT",
+            href: "/Contact",
+        },
+    ];
+
+    let isSheetOpen = false;
+</script>
+
+<nav
+class="text-secondary-foreground py-14 px-6 flex justify-between items-center mx-auto"
+>
+    <div>
+        <Button href="/" variant="primary">
+            <svg
+                class=" w-12 h-auto ms-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 256 256"
+                ><path
+                    fill="currentColor"
+                    d="M240 128a15.79 15.79 0 0 1-10.5 15l-63.44 23.07L143 229.5a16 16 0 0 1-30 0l-23.06-63.44L26.5 143a16 16 0 0 1 0-30l63.44-23.06L113 26.5a16 16 0 0 1 30 0l23.07 63.44L229.5 113a15.79 15.79 0 0 1 10.5 15"
+                /></svg
+            >
+        </Button>
+    </div>
+    <!-- icon check -->
+    <Sheet.Root bind:open={isSheetOpen}>
+        <div class=" flex me-2">
+                <ThemeSwitch  />
+            <Sheet.Trigger>
+                <Button size="icon" variant="primary">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="w-10 h-auto"
+                        viewBox="0 0 24 24"
+                        ><g fill="none"
+                            ><path
+                                d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"
+                            /><path
+                                fill="currentColor"
+                                d="M20 18a1 1 0 0 1 .117 1.993L20 20H4a1 1 0 0 1-.117-1.993L4 18zm0-7a1 1 0 1 1 0 2H4a1 1 0 1 1 0-2zm0-7a1 1 0 1 1 0 2H4a1 1 0 0 1 0-2z"
+                            /></g
+                        ></svg
+                    >
+                </Button></Sheet.Trigger
+            >
+        </div>
+        
+        <Sheet.Content class="flex flex-col justify-center">
+            {#each links as link}
+                <Button
+                    class=" text-xl {$page.url.pathname == link.href &&
+                        'bg-primary text-secondary'}"
+                    href={link.href}
+                    variant=" "
+                    on:click={() => (isSheetOpen = false)}
+                    >{link.text}
+                </Button>
+            {/each}
+        </Sheet.Content>
+    </Sheet.Root>
+</nav>
+
--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,1 +1,31 @@
-About me
+<script>
+ import Button from "$lib/components/ui/button/button.svelte";
+  </script>
+
+<div class="flex justify-end">
+    <div class="bg-secondary min-h-[calc(100vh-10rem)] w-[60vw] relative md:">
+      <div class="bg-[#fff9ef] shadow-[0_10px_60px_-15px_rgba(0,0,0,0.7)] w-[20rem] absolute start-10 -translate-x-full translate-y-1/4 flex flex-col items-center gap-2 justify-between ">
+     <img src="/profilei.png" class="rounded-full w-60" alt="personal picture" />
+    <div class="flex flex-col items-center gap-2">
+      <h1 class=" font-bold text-4xl text-[#4c6763] ">
+        Rahaf
+            </h1>
+            <hr class="w-20 border-1 border-[#4c6763]">
+            <h2 class="text-[#4c6763]">
+              Software Engineer Student
+            </h2>
+    </div>
+<div class="w-full flex justify-center justify-evenly p-2 mt-6">
+  <Button variant="circle" size="icon" href="https://x.com/imrahaf_5">
+  <svg xmlns="http://www.w3.org/2000/svg" class=" w-4 fill-background" viewBox="0 0 24 24"><path  d="m17.687 3.063l-4.996 5.711l-4.32-5.711H2.112l7.477 9.776l-7.086 8.099h3.034l5.469-6.25l4.78 6.25h6.102l-7.794-10.304l6.625-7.571zm-1.064 16.06L5.654 4.782h1.803l10.846 14.34z"/></svg>
+</Button>
+
+<Button variant="circle" size="icon" href="">
+  <svg xmlns="http://www.w3.org/2000/svg" class=" w-4 fill-background" viewBox="0 0 24 24"><path  d="M18.336 18.339h-2.665v-4.177c0-.996-.02-2.278-1.39-2.278c-1.389 0-1.601 1.084-1.601 2.205v4.25h-2.666V9.75h2.56v1.17h.035c.358-.674 1.228-1.387 2.528-1.387c2.7 0 3.2 1.778 3.2 4.092v4.714M7.004 8.575a1.546 1.546 0 0 1-1.548-1.549a1.548 1.548 0 1 1 1.547 1.549m1.336 9.764H5.667V9.75H8.34zM19.67 3H4.33C3.594 3 3 3.58 3 4.297v15.406C3 20.42 3.594 21 4.328 21h15.339C20.4 21 21 20.42 21 19.703V4.297C21 3.581 20.4 3 19.666 3z"/></svg>
+</Button>
+
+</div>
+      </div>
+    </div>
+</div>
+
