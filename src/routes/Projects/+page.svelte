40e61ae5329b935c0835577e63b9d4af64e6bbc7
Rahaf <roofaj1029@gmail.com>
2024-12-02 21:45:32 +0300

    test

--- src/routes/Projects/+page.svelte
+++ src/routes/Projects/+page.svelte
@@ -11,7 +11,7 @@
    <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero et aperiam qui veniam nisi sequi, omnis architecto magnam magni nobis quis numquam ut veritatis iste ipsa excepturi, consequuntur similique dolorem?
    </p>
-  <ProjectCard title="Rahaf" subtitle="Software Engineer Student" imageSrc="/project1.jpg">
+  <ProjectCard title="Calculator" subtitle="Software Engineer Student" imageSrc="/project1.jpg">
     <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur id illum quibusdam suscipit quidem quis adipisci et ut, aliquam cum, deleniti labore quaerat, reiciendis quo enim sunt fugiat voluptatibus. Quos.
     </p>
